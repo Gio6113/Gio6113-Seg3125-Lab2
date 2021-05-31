74eead28e34137ae720616711b37ead8b612363d
Giorgio Sawaya <giorgiosawaya@gmail.com>
2021-05-31 00:31:33 -0400

    basic look done

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,44 @@
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
 
-function openInfo(evt, tabName) {
+// function openInfo(evt, tabName) {
 
-	// Get all elements with class="tabcontent" and hide them
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
+// 	// Get all elements with class="tabcontent" and hide them
+// 	tabcontent = document.getElementsByClassName("tabcontent");
+// 	for (i = 0; i < tabcontent.length; i++) {
+// 		tabcontent[i].style.display = "none";
+// 	}
 
-	// Get all elements with class="tablinks" and remove the class "active"
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
+// 	// Get all elements with class="tablinks" and remove the class "active"
+// 	tablinks = document.getElementsByClassName("tablinks");
+// 	for (i = 0; i < tablinks.length; i++) {
+// 		tablinks[i].className = tablinks[i].className.replace(" active", "");
+// 	}
 
-	// Show the current tab, and add an "active" class to the button that opened the tab
-	document.getElementById(tabName)[0].style.display = "block";
-	evt.currentTarget.className += " active";
+// 	// Show the current tab, and add an "active" class to the button that opened the tab
+// 	document.getElementById(tabName)[0].style.display = "block";
+// 	evt.currentTarget.className += " active";
 
+// }
+
+function openProducts(){
+ 	document.getElementsByClassName("product")[0].style.display = "block";
+	document.getElementsByClassName("cart")[0].style.display = "none";
+	document.getElementsByClassName("selection")[0].style.display = "none";
 }
 
+function openSelections(){
+   document.getElementsByClassName("product")[0].style.display = "none";
+   document.getElementsByClassName("cart")[0].style.display = "none";
+   document.getElementsByClassName("selection")[0].style.display = "block";
+}
+
+function openCart(){
+	document.getElementsByClassName("product")[0].style.display = "none";
+	document.getElementsByClassName("cart")[0].style.display = "block";
+	document.getElementsByClassName("selection")[0].style.display = "none";
+
+ }
 
 
 var bigger = false ;
